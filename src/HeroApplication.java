4ebf08a6b725e359348be30640a2cad3f359c8e2
shegoestech202122 <artjoms.v.petrovs@gmail.com>
2021-07-14 17:22:24 +0300

    Added Conditionals logic

--- src/HeroApplication.java
+++ src/HeroApplication.java
@@ -49,5 +49,44 @@ public class HeroApplication {
         System.out.println("***************************");
         System.out.println("*** HERO INFORMATION ***");
         System.out.println("***************************");
+
+
+
+
+
+        // If a hero is a villain or a hero
+        if(isEvil) {
+            System.out.println( " Hero is a villain. he is stealing cookies!!!");
+        } else {
+            System.out.println("hero protects the city!");
+        }
+
+
+        heroCity = "Metropolis";
+
+        switch (heroCity) {
+            case "New York":
+                heroSalary = 2000;
+                break;
+            case "Riga":
+                heroSalary = 1200.91;
+                break;
+            case "Metropolis":
+                heroSalary = 1000000;
+                break;
+            default:
+                heroSalary = 100;
+        }
+
+        System.out.println("Hero's new salary: " + heroSalary);
+        // How many cookies can superhero purchase for his /her salary
+        // 1 cookie costs = 1.29 euro
+        System.out.println("**********************************");
+        System.out.println("********** Financials ***************");
+        int numOfCookies = (int)Math.floor(heroSalary / 1.29);
+        System.out.println(" Hero can purchase " + numOfCookies + " cookies");
+        double averageDailySalary = Math.round( (heroSalary / 30 * 100)  ) / 100;
+        System.out.println(" Hero average daily salary is " + averageDailySalary + " euro");
+
     }
 }
